027cdcf4a2d0ab222883fe7734c906ad59a06907
Lucas Alves <lucasdolohov@gmail.com>
2023-09-02 02:27:20 -0300

    feat: rota de modificação de usuário

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 const knex = require("../database/knex");
 const { Errors } = require("../utils/errorHandling");
 
@@ -15,11 +15,53 @@ class UsersControllers {
 
     const hashedPassword = await hash(password, 8);
 
-    const userRegistered = await knex("users").insert({
+    await knex("users").insert({
       name,
       email,
       password: hashedPassword,
     });
+
+    return res.status(201).json();
+  }
+
+  async update(req, res) {
+    const { name, email, password, old_password: oldPassword } = req.body;
+    const { id } = req.params;
+
+    const user = await knex("users").where({ id }).first();
+
+    if (!user) {
+      throw new Errors("Usuario não encontrado", 404);
+    }
+
+    const userWithEmailToUpdate = await knex("users").where({ email }).first();
+
+    if (userWithEmailToUpdate && userWithEmailToUpdate.id !== user.id) {
+      throw new Errors("Este e-mail já está em uso");
+    }
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if (password && !oldPassword) {
+      throw new Errors(
+        "Você precisa informar a senha antiga para definir a nova senha"
+      );
+    }
+
+    if (password && oldPassword) {
+      const checkOldPassword = await compare(oldPassword, user.password);
+
+      if (!checkOldPassword) {
+        throw new Errors("A senha antiga não confere");
+      }
+
+      user.password = await hash(password, 8);
+    }
+
+    await knex("users")
+      .update({ ...user })
+      .where({ id });
 
     return res.status(201).json();
   }
