54011325305ba372d76b22c4583d9cc28197a18c
Lucas Alves <lucasdolohov@gmail.com>
2023-09-02 04:29:08 -0300

    feat: rota de criação de notas

---
+++ src/controllers/MovieNotesController.js
@@ -0,0 +1,38 @@
+const knex = require("../database/knex");
+const { Errors } = require("../utils/errorHandling");
+
+class MovieNotesController {
+  async create(req, res) {
+    const { title, description, rating, tags } = req.body;
+    const { user_id } = req.params;
+
+    const user = await knex("users").where({ id: user_id }).first();
+
+    if (!user) {
+      throw new Errors("Usuario não encontrado", 404);
+    }
+
+    if (rating < 0 || rating > 10) {
+      throw new Errors("O rating deve ser um número de 1 à 10");
+    }
+
+    const [note_id] = await knex("movie_notes").insert({
+      title,
+      description,
+      rating,
+      user_id,
+    });
+
+    const tagsInsert = tags.map((name) => ({
+      note_id,
+      name,
+      user_id,
+    }));
+
+    await knex("movie_tags").insert(tagsInsert);
+
+    return res.status(201).json();
+  }
+}
+
+module.exports = MovieNotesController;
--- src/database/knex/migrations/20230821070532_createMovieTags.js
+++ src/database/knex/migrations/20230821070532_createMovieTags.js
@@ -1,11 +1,9 @@
 exports.up = (knex) =>
   knex.schema.createTable("movie_tags", (table) => {
     table.increments("id");
+    table.text("name");
     table.integer("note_id").references("id").inTable("movie_notes").onDelete("CASCADE");
     table.integer("user_id").references("id").inTable("users");
-
-    table.timestamp("created_at").default(knex.fn.now());
-    table.timestamp("updated_at").default(knex.fn.now());
   });
 
 exports.down = (knex) => knex.schema.dropTable("movie_tags");
