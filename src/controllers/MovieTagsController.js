6963af43b0e00e6123f196f358481b3a036a1ed8
Lucas Alves <lucasdolohov@gmail.com>
2023-09-03 00:02:27 -0300

    feat: rota de buscar tags por usuario

---
+++ src/controllers/MovieTagsController.js
@@ -0,0 +1,13 @@
+const knex = require("../database/knex");
+
+class MovieTagsController {
+  async readTagsByUser(req, res) {
+    const { user_id } = req.params;
+
+    const tags = await knex("movie_tags").where({ user_id });
+
+    return res.json(tags);
+  }
+}
+
+module.exports = MovieTagsController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 
 const usersRoutes = require("./users.routes");
 const movieNotesRoutes = require("./movieNotes.routes");
+const movieTagsRoutes = require("./movieTags.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRoutes);
 routes.use("/movie-notes", movieNotesRoutes);
+routes.use("/movie-tags", movieTagsRoutes);
 
 module.exports = routes;
