ec783180183434b075e890eb008f2abf86465329
Lucas Alves <lucasdolohov@gmail.com>
2023-09-22 00:09:51 -0300

    feat: cors

--- src/server.js
+++ src/server.js
@@ -1,16 +1,18 @@
-require("express-async-errors");
-const express = require("express");
+require('express-async-errors');
+const express = require('express');
+const cors = require('cors');
 
-const { errorHandling } = require("./utils/errorHandling");
-const { UPLOAD_FOLDER } = require("./configs/upload");
+const { errorHandling } = require('./utils/errorHandling');
+const { UPLOAD_FOLDER } = require('./configs/upload');
 
-const routes = require("./routes");
+const routes = require('./routes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use('/files', express.static(UPLOAD_FOLDER))
+app.use('/files', express.static(UPLOAD_FOLDER));
 
 app.use(errorHandling);
 
